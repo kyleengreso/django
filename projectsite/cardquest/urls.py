117551fcce70be2951e0db2c5058d9d64bfd6c80
superbiko1 <202280348@psu.palawan.edu.ph>
2023-12-12 21:18:07 +0800

    edit files

--- projectsite/cardquest/urls.py
+++ projectsite/cardquest/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('trainer', views.TrainerList.as_view(), name='trainer-list'),
     path('pokemon_card', views.PokemonCardList.as_view(), name='pokemon-card'),
     path('collection', views.CollectionList.as_view(), name='collection'),
     path('trainer_list', TrainerList.as_view(), name='trainer-list'),
